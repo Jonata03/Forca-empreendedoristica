e2fa98ea8ae02c016a8669560a6ab289940acc57
Jonata <jonatanery2016@gmail.com>
2023-11-27 15:19:30 -0400

    adicionado o sistema de erro e acertos de letras

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const cabeca = document.querySelector(".cabeca");
+    const corpo = document.querySelector(".corpo")
+    const bracoDireito = document.querySelector(".braco-direito")
+    const bracoEsquerdo = document.querySelector(".braco-esquerdo")
+    const pernaDireita = document.querySelector(".perna-direita")
+    const pernaEsquerda = document.querySelector(".perna-esquerda")
+    const forca = document.querySelector('#forca');
+
+    const palavras = ["Empreendedorismo", "oportunidade", "negócios", "inovadocao", "desafios", "criatividade", "resiliencia", "visao", "estrategia", "liderança", "adaptacao", "constancia", "crescimento", "sustentavel", "persistencia", "sucesso", "inspiracao", "planejamento", "perseveranca", "coragem", "inovacao", "mercado", "competitividade", "liderar", "empreender", "transformacao", "confianca", "proatividade", "excelencia"]
+
+    var letraTeclado = document.querySelectorAll('.letras');
+
+    const numero = Math.floor(Math.random() * 29);
+    var letra = palavras[numero].split("");
+    const palavraSecreta = document.querySelector("#palavraSecreta");
+
+    for (let i = 0; i < letra.length; i++) {
+        palavraSecreta.innerHTML += '-'
+    };
+    const estiloCabeca = getComputedStyle(cabeca);
+    const estiloCorpo = getComputedStyle(corpo);
+    const estiloBracoDireito = getComputedStyle(bracoDireito);
+    const estiloBracoEsquerdo = getComputedStyle(bracoEsquerdo);
+    const estiloPernaEsquerda = getComputedStyle(pernaDireita);
+    const estiloPernaDireita = getComputedStyle(pernaEsquerda);
+ let erros = 0;
+    letraTeclado.forEach(function (letraClicada) {
+        letraClicada.addEventListener('click', function () {
+            let clicada = letraClicada.innerText;
+            let clicadaMinuscula = clicada.toLowerCase()
+            var novaPalavra = "";
+           
+            let acertou = false;
+            for (let i = 0; i < letra.length; i++) {
+                if (palavraSecreta.innerText[i] !== "-") {
+                    novaPalavra += palavraSecreta.innerText[i];
+                } else if (clicadaMinuscula === letra[i]) {
+                    // Se a letra clicada corresponder, substitua
+                    novaPalavra += clicadaMinuscula;
+                    acertou = true;
+                } else {
+                    // Caso contrário, mantenha o "-"
+                    novaPalavra += "-";
+                }
+            }
+            palavraSecreta.innerText = novaPalavra;
+            if (!acertou){
+                erros++;
+                if (erros === 1) {
+                    cabeca.style.backgroundColor = 'black'
+                }
+                else if (erros === 2) {
+                    corpo.style.backgroundColor = 'black'
+                }
+                else if (erros === 3) {
+                    bracoEsquerdo.style.backgroundColor = 'black';
+                }
+                else if (erros === 4) {
+                    bracoDireito.style.backgroundColor = 'black'
+                }
+                else if (erros === 5) {
+                    pernaDireita.style.backgroundColor = 'black'
+                }
+
+                else if (erros === 6) {
+                    pernaEsquerda.style.backgroundColor = 'black'
+                    
+                }
+            }
+        });
+
+    })
+
+})
